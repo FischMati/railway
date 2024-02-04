5436d564ab2628588252c515db44bef03bd9a9f7
FischMati <22280040+FischMati@users.noreply.github.com>
2024-02-04 18:18:45 +0000

    Enabling basic logging

---
+++ middleware.js
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export async function middleware({ nextUrl }) {
+  const response = NextResponse.next();
+  if(!nextUrl?.href?.includes("_next")){
+    console.log(nextUrl);
+  }
+  return response;
+}
