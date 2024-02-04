59de7bf716a9685901a8e1caa68c22809e264610
FischMati <22280040+FischMati@users.noreply.github.com>
2024-02-04 18:22:35 +0000

    simplifying logging

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function middleware({ nextUrl }) {
   const response = NextResponse.next();
   if(!nextUrl?.href?.includes("_next")){
-    console.log(nextUrl);
+    console.log(nextUrl.href);
   }
   return response;
 }
